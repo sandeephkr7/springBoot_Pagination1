d8c43713003318a22d74afc0e47af9e8cd0184aa
sandeep <sandeep@DESKTOP-LI97J0F>
2019-06-07 14:02:15 +0530

    Third Commit

--- TestProject/src/com/alighthub/model/Employee.java
+++ TestProject/src/com/alighthub/model/Employee.java
@@ -1,5 +1,35 @@
 package com.alighthub.model;
 
-public class Employee {
-
+public class Employee 
+{
+	private int empid;
+	private String empname;
+	private String address;
+	private String email;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
